603fe925a4a82361ab146ba32e1a89b7cc402726
Mahziyar <mhzrerfani@gmail.com>
2023-01-18 13:41:39 +0330

    Solved Day 11 Part 1

--- README.md
+++ README.md
@@ -10,3 +10,4 @@
 8. [Treetop Tree House](https://adventofcode.com/2022/day/8)
 9. [Rope Bridge](https://adventofcode.com/2022/day/9)
 10. [Cathode-Ray Tube](https://adventofcode.com/2022/day/10)
+11. [Monkey in the Middle](https://adventofcode.com/2022/day/11)
---
+++ day-11/index.js
@@ -0,0 +1,56 @@
+// Run in browser console on the input page
+// https://adventofcode.com/2022/day/11/input
+
+const input = document
+  .querySelector("pre")
+  .innerText.split("\n\n")
+  .map((monkey) => {
+    const array = monkey.split("  ").map((n, i) => {
+      if (i === 1) n = n.split(":")[1].split(",").map(Number);
+      if (i === 2) n = n.split("= ")[1].slice().replace("\n", "").split(" ");
+      if (i === 3) n = n.split(" ").map(Number)[3];
+      if (i === 5 || i === 7) n = n.split(" ").map(Number)[5];
+      if (i === 0 || i === 4 || i === 6) return;
+      return n;
+    });
+    const [items, operation, test, onTrue, onFalse] = array.filter(
+      (e) => e !== undefined
+    );
+    return {
+      items,
+      operation,
+      test,
+      nextMonkey: [onTrue, onFalse],
+      inspect: 0,
+    };
+  });
+
+const getPart1 = () => {
+  const monkeys = [...input];
+  for (let i = 1; i <= 20; i++) {
+    monkeys.forEach((monkey) => {
+      while (monkey.items.length >= 1) {
+        const operation = monkey.operation
+          .map((n) => (n === "old" ? monkey.items[0] : n))
+          .join("");
+
+        monkey.items[0] = Math.trunc(eval(operation) / 3);
+
+        monkey.items[0] % monkey.test === 0
+          ? monkeys[monkey.nextMonkey[0]].items.push(monkey.items.shift())
+          : monkeys[monkey.nextMonkey[1]].items.push(monkey.items.shift());
+        monkey.inspect += 1;
+      }
+    });
+  }
+
+  const inspectTimes = monkeys
+    .map((monkey) => monkey.inspect)
+    .sort((a, b) => b - a);
+  console.log(inspectTimes);
+
+  return inspectTimes[0] * inspectTimes[1];
+};
+
+const part1 = getPart1();
+console.log(part1);
